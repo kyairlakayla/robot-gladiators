aaba3542f28d50bc493892c86d40b3b42b5cc6f9
Kayla <kyairlakayla.davis@gmail.com>
2021-07-06 13:03:40 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,11 +3,13 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+for(var i = 0; i < enemyNames.length; i++) {
+}
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting the round 
     window.alert("welcome to Robot Gladiators!");
     var promptFight = window.prompt("would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose. ");
@@ -57,4 +59,6 @@ else {
     fight();
   }
 }    
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
